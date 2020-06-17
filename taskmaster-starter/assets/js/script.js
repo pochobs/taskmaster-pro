116cca25f610ab34ccffb6f72e0096ac8746e70e
Pochobs <pochobs@hotmail.com>
2020-06-17 11:17:30 -0500

    Module 5.3.5 done, drag and drop added

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -35,7 +35,37 @@ $(".card .list-group").sortable({
     console.log("out", event.target);
   },
   update: function(event) {
-    console.log("update", this);
+  // array to store the task data in
+var tempArr = [];
+
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+});
+
+console.log(tempArr);
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
   }
 });
 var loadTasks = function() {
